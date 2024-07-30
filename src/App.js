909588795a427dd5678441ad21d7fd4cf611f578
manu-git-hub <124423662+manu-git-hub@users.noreply.github.com>
2024-07-30 22:11:29 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import LandingPage from './LandingPage';
+import WhatAreWe from './components/WhatAreWe';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+     
+    </div>
+  );
+}
+
+export default App;
